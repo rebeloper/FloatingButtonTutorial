a52670aa77cec305bd93ed378c8f23a7dd54b1b9
Alex Nagy <therebeloper@gmail.com>
2021-01-21 13:08:48 +0200

    Initial Commit

---
+++ FloatingButtonTutorial/FloatingButtonTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingButtonTutorialApp.swift
+//  FloatingButtonTutorial
+//
+//  Created by Alex Nagy on 21.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct FloatingButtonTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
